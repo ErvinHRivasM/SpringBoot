3dee7df4e07b44bd64bba5cfae94c1cf48a68b13
ervin_h25@hotmail.com <ervin_h25hotmail.com@erivas.local>
2024-05-15 10:22:24 -0500

    endpoint separation project

--- fundamentos/src/main/java/com/fundamentosSpringboot/fundamentos/bean/MyBeanImplement.java
+++
@@ -1,9 +0,0 @@
-package com.fundamentosSpringboot.fundamentos.bean;
-
-public class MyBeanImplement implements MyBean {
-
-    @Override
-    public void print() {
-        System.out.println("My primer Bean sin intermediarios..");
-    }
-}
