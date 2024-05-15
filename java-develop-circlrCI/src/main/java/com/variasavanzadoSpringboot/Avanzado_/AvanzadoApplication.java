ea5edbaae1e297ec70e107d91066311847352a3c
ervin_h25@hotmail.com <ervin_h25hotmail.com@erivas.local>
2024-05-15 10:35:46 -0500

    code for circleCI Testing

---
+++ java-develop-circlrCI/src/main/java/com/variasavanzadoSpringboot/Avanzado_/AvanzadoApplication.java
@@ -0,0 +1,13 @@
+package com.variasavanzadoSpringboot.Avanzado_;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AvanzadoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AvanzadoApplication.class, args);
+	}
+
+}
